078aee8a289c924fd9da9b66495b079003aef455
raunchard <reinhard.zach@gmail.com>
2023-03-26 21:22:28 +0300

    add 404

--- index.js
+++ index.js
@@ -40,4 +40,8 @@ app.post("/explaincode", (req, res) => {
 		);
 });
 
+app.use((req, res, next) => {
+	res.status(404).send("Sorry can't find that!");
+});
+
 app.listen(port, () => console.log(`Server running on port ${port}...`));
